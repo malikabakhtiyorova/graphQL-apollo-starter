3e08401a0a46340e7412de5ab63120ed9fb7c6a3
Malika Bakhtiyorova <malikabakhtiyorova@yandex.ru>
2021-02-08 16:13:17 +0500

    struct type

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const PORT = process.env.PORT || 3000;
 const typeDefs = gql(`
 type Query {
     version: Float!
+    users: [User!]!
 } 
+type User {
+    id: Int!
+    username: String!
+    age: Int
+}
 `)
+
+const users = [
+    {id: 1, username: 'user1', age: 5},
+    {id: 2, username: 'user2', age: null}
+]
 
 const resolvers = {
     Query: {
         version: () =>1.0,
+        users: () => users
     },
 }
 
